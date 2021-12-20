dd0f985e913aefc845acb7839a488b32d42434e3
xinjiawei <xinjiawei@outlook.com>
2021-12-20 13:03:58 +0800

    02

--- test.cpp
+++ test.cpp
@@ -2,40 +2,52 @@
 #include<string.h>
 #include<stdlib.h>
 
-typedef struct{
-	int weight; 
-	char Data; 													//存放节点的字符 
-	int Parent,Lchild,Rchild; 
-}HTNode,*HuffmanTree; 
+#include "windows.h"
 
-typedef char** HuffmanCode; 
-void HuffmanCoding(HuffmanTree &,char *,int *,int);
-void select(HuffmanTree HT,int j,int *s1,int *s2);   //选择父母为 0且权值最小的两个结点 
-void Initialization(); 										   // 初始化赫夫曼树 
-void Coding(); 														//赫夫曼编码  
-void Decoding(); 													//赫夫曼译码
-void find(HuffmanTree &HT,char *code,char *text,int i,int m);
-HuffmanTree HT; 
-int n=0; 												  //赫夫曼树叶子结点数目
 
-//------------------------------------------------------------------------------
-																		//主函数 
 
-int	main()
-{
-	char T;
-	while (1)
-	{	
-		printf("\n");
-		printf("\t\t\t\t-----------------赫夫曼编码/译码器-----------------\n");
-		printf("\t\t\t\t建立赫夫曼树-------------------------------------I\n");
-		printf("\t\t\t\t进行赫夫曼编码-----------------------------------C\n");
-		printf("\t\t\t\t进行赫夫曼译码-----------------------------------D\n");
-		printf("\t\t\t\t退出此程序---------------------------------------Q\n");
-		printf("\t\t\t\t请输入（I C D Q）                          \n\t\t\t\t"); 
-		scanf("%c",&T);	
-		switch(T)			
-		{							
+
+	typedef struct
+	{
+		int weight;
+		char Data; //存放节点的字符
+		int Parent, Lchild, Rchild;
+	} HTNode, *HuffmanTree;
+
+	typedef char **HuffmanCode;
+	void HuffmanCoding(HuffmanTree &, char *, int *, int);
+	void select(HuffmanTree HT, int j, int *s1, int *s2); //选择父母为 0且权值最小的两个结点
+	void Initialization();								  // 初始化赫夫曼树
+	void Coding();										  //赫夫曼编码
+	void Decoding();									  //赫夫曼译码
+	void find(HuffmanTree & HT, char *code, char *text, int i, int m);
+	HuffmanTree HT;
+	int n = 0; //赫夫曼树叶子结点数目
+
+	void set_console_color(unsigned short color_index)
+	{
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color_index);
+	}
+	//------------------------------------------------------------------------------
+	//主函数
+
+	int main()
+	{
+		char T;
+		while (1)
+		{
+			printf("\n");
+			set_console_color(1);
+			printf("\t\t\t\t-----------------赫夫曼编码/译码器-----------------\n");
+			set_console_color(6);
+			printf("\t\t\t\t建立赫夫曼树-------------------------------------I\n");
+			printf("\t\t\t\t进行赫夫曼编码-----------------------------------C\n");
+			printf("\t\t\t\t进行赫夫曼译码-----------------------------------D\n");
+			printf("\t\t\t\t退出此程序---------------------------------------Q\n");
+			printf("\t\t\t\t请输入（I C D Q）                          \n\t\t\t\t");
+			scanf("%c", &T);
+			switch (T)
+			{							
 			case 'I':				
 				Initialization();				
 				break ;				
@@ -62,10 +74,17 @@ void Initialization()
 {
 	
 	FILE *fp;
-	int	i,w[52];												//存放字符的权值 
-	char character[52];											 // 存放 n个字符
+
+	set_console_color(2); // 存放 n个字符
 	printf("\t\t\t\t请输入字符的个数:");	
-	scanf("%d",&n);			
+	scanf("%d",&n);
+
+	int i, w[2*n + 1]; //存放字符的权值,动态存储
+	char character[2*n + 1]; //动态存储
+	//int i, w[n + 1];		 //存放字符的权值，静态存储
+	//char character[n + 1];	 //静态存储
+
+	set_console_color(2);
 	printf("\t\t\t\t请输入%d个字符及权值:\n\t\t\t\t" ,n);
 	for(i=0;i<n;i++)					
 	{					
@@ -74,8 +93,8 @@ void Initialization()
 		scanf("%d",&w[i]);								 // 输入字符和对应的权值
 	}					
 	HuffmanCoding(HT,character,w,n);							     // 建立赫夫曼树
-	if ((fp=fopen("hfmTree.txt","w"))==NULL) 
-		printf("\t\t\t\thfmTree.txt	打开失败 \n"  );	
+	if ((fp=fopen("data.txt","w"))==NULL)
+		printf("\t\t\t\tdata.txt	打开失败 \n"  );	
 	for(i=1;i<=2*n-1;i++)				
 	{					
 		if(fwrite(&(HT[i].Data),sizeof (char),1,fp)!=1)
@@ -88,8 +107,9 @@ void Initialization()
 			printf(	"\t\t\t\t写入文件失败  \n");	
 		if(fwrite(&(HT[i].Rchild),sizeof(int),1,fp)!=1)
 			printf(	"\t\t\t\t写入文件失败  \n");		
-	}					
-	printf("\t\t\t\t赫夫曼树已建立，并已存于 hfmTree.txt\n");
+	}
+	set_console_color(3);
+	printf("\t\t\t\t赫夫曼树已建立，并已存于 <<<<data.txt>>>>\n");
 	fclose(fp);					
 
 }
@@ -191,18 +211,24 @@ void Coding()
 		if((fw=fopen("CodeFile.txt","wb+"))==NULL) 	 // 建立新文件进行读/ 写
 			printf("\t\t\t\tCodeFile.txt 打开失败 \n");
 		fscanf(fp,"%c",&temp);	// 从fp 所指的文件中读入第一个字符到	temp变量
+		printf("\n\t\t\t\t 编码为：\n");
 		while(!feof(fp))
 		{
 			for(i=1;i<=n;i++)
 				if (HT[i].Data==temp)
-					break ;						   // 若和 1号节点相同则跳出循环
+					break ;				// 若和 1号节点相同则跳出循环
 			for(r=0;HC[i][r]!='\0';r++)	// 在赫夫曼树中查找字符所在的位置
-				fputc(HC[i][r],fw);	// 将字符对应的编码存入文件，把	HC[i][r]	写到 fw所指示文件
-			fscanf(fp,"%c",&temp);					 // 从文件读入下一个字符
+			{
+				fputc(HC[i][r],fw);	// 将字符对应的编码存入文件，把	HC[i][r] 写到 fw所指示文件
+			 	set_console_color(4);
+				printf("\n\t\t\t\t %c", HC[i][r]);
+			}
+				fscanf(fp, "%c", &temp); // 从文件读入下一个字符
 		}
 		fclose(fw);
 		fclose(fp);
-		printf("\n\t\t\t\thfmTree.txt	已编码成功，存于CodeFile.txt\n"	);
+		set_console_color(3);
+		printf("\n\t\t\t\t 根据data.txt	将ToBeTran.txt 编码成功，存于<<<<CodeFile.txt>>>>\n");
 }
 
 //------------------------------------------------------------------------------
@@ -237,7 +263,8 @@ void Decoding()
 		fputc(p[i],fw);						   	// 把 p[i]写到 fw所指示的文件中
 	fclose(fp);
 	fclose(fw);
-	printf(	"\n\t\t\t\tCodeFile.txt	译码成功，存于Textfile.txt\n"	);
+	set_console_color(3);
+	printf("\n\t\t\t\t 根据data.txt 将CodeFile.txt 译码成功，存于<<<<Textfile.txt>>>>\n");
 }
 
 //------------------------------------------------------------------------------
